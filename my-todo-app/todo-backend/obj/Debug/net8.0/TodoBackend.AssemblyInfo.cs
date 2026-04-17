8ca0d61adee89807f85b0196ded7fade6bf16abf
Tewter73 <tewkunanon73@gmail.com>
2026-04-17 17:52:16 +0700

    Bump assembly informational version
    Update AssemblyInformationalVersion from 1.0.0+5ea06b769a5d... to 1.0.0+152d89ae796c... and update related build metadata (AssemblyInfoInputs.cache, SourceLink). Rebuild updated bin/Debug and obj/Debug outputs (DLL/EXE/PDB/apphost/ref/refint). No functional source changes.

--- my-todo-app/todo-backend/obj/Debug/net8.0/TodoBackend.AssemblyInfo.cs
+++ my-todo-app/todo-backend/obj/Debug/net8.0/TodoBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea06b769a5d6c3a7560d0bfb58414cf843484c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152d89ae796c1d0510ebfe0a263a3343394a6c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
